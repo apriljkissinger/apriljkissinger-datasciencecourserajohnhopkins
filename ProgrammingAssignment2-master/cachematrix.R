860f3b2595ee18924feb6c9bb83e71f9cb45da82
A Kissinger <apriljkissinger@gmail.com>
2020-04-26 22:42:52 -0500

    Added the two functions makeCacheMatrix and cacheSOlve to the file cachematrix.R to complete the assignment

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,32 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+rm (list =ls())
+
+makeCacheMatrix <- function(x = matrix()){
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function (solve) m <<- solve
+        getinv <- function () m
+        list(set = set, get = get, setinv = setinv, getinv = getinv )
+}
+
+## Returns a matrix that it is the inverse of mat.
+
+cacheSolve <- function(x, ...) {
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
+}
